f7f5de5dbf2a2761261a3b13ca95e14a609d7010
aokiyuya <yuya.aoki@fujielab.org>
2016-08-05 16:19:47 +0900

    upload test.dll

---
+++ system/testdir/kinoko_access.cs
@@ -0,0 +1,18 @@
+namespace KinokoLib
+{
+	public class AccessClass
+	{
+		public static string push(string status_name, string status)
+		{
+			return "SUCCESS";
+		}
+		public static string pull(string status_name)
+		{
+			return "string";
+		}
+		public static bool update(string module_name, string status_name, long time)
+		{
+			return true;
+		}
+	}
+}
--- system/testdir/test.cs
+++ system/testdir/test.cs
@@ -1,24 +1,14 @@
-using UtilityMethods;
+using KinokoLib;
 
 class TestCode
 {
     static void Main(string[] args) 
     {
-        System.Console.WriteLine("Calling methods from MathLibrary.DLL:");
-
-        if (args.Length != 2)
-        {
-            System.Console.WriteLine("Usage: TestCode <num1> <num2>");
-            return;
-        }
-
-        long num1 = long.Parse(args[0]);
-        long num2 = long.Parse(args[1]);
-
-        long sum = AddClass.Add(num1, num2);
-        long product = MultiplyClass.Multiply(num1, num2);
-
-        System.Console.WriteLine("{0} + {1} = {2}", num1, num2, sum);
-        System.Console.WriteLine("{0} * {1} = {2}", num1, num2, product);
+		string test = "test";
+		string status = "status";
+		string module = "module";
+		AccessClass.push(test, status);
+		AccessClass.pull(test);
+		AccessClass.update(module, test, -1);
     }
 }
