80c7bc377e66c2a2617e9629e2a327710b3904d1
aokiyuya <yuya.aoki@fujielab.org>
2016-08-05 13:44:54 +0900

    add testcodes and dll

---
+++ system/testdir/Add.cs
@@ -0,0 +1,13 @@
+
+namespace UtilityMethods
+{
+	public class AddClass
+	{
+		public static long Add(long i, long j)
+		{
+			return (i + j);
+		}
+	}
+}
+
+
---
+++ system/testdir/Multi.cs
@@ -0,0 +1,13 @@
+
+namespace UtilityMethods
+{
+	public class MultiplyClass
+	{
+		public static long Multiply(long x, long y)
+		{
+			return (x * y);
+		}
+	}
+}
+
+
