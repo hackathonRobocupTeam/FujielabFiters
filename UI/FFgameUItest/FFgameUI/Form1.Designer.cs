6ede462b745dd8ec6ec1bcbc211a21e7c23c531c
shibata-mamoru <mamoru.shibata@fujielab.org>
2016-08-06 17:42:20 +0900

    大まかなUI表示部分完成

--- UI/FFgameUItest/FFgameUI/Form1.Designer.cs
+++ UI/FFgameUItest/FFgameUI/Form1.Designer.cs
@@ -29,6 +29,10 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.button1 = new System.Windows.Forms.Button();
             this.label6 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
@@ -40,13 +44,13 @@
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.panel3 = new System.Windows.Forms.Panel();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
             this.panel1.Controls.Add(this.panel3);
             this.panel1.Controls.Add(this.panel2);
             this.panel1.Controls.Add(this.button1);
@@ -66,6 +70,44 @@
             this.panel1.TabIndex = 0;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(397, 579);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "testguard";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(200, 579);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(89, 29);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "testattack";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.AutoSize = true;
+            this.panel3.Location = new System.Drawing.Point(841, 143);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(360, 335);
+            this.panel3.TabIndex = 12;
+            this.panel3.Paint += new System.Windows.Forms.PaintEventHandler(this.panel3_Paint_1);
+            // 
+            // panel2
+            // 
+            this.panel2.AutoSize = true;
+            this.panel2.Location = new System.Drawing.Point(122, 143);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(360, 335);
+            this.panel2.TabIndex = 11;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("MS UI Gothic", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
@@ -80,8 +122,9 @@
             // label6
             // 
             this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.ControlDark;
             this.label6.Font = new System.Drawing.Font("MS UI Gothic", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label6.Location = new System.Drawing.Point(772, 498);
+            this.label6.Location = new System.Drawing.Point(772, 489);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(63, 35);
             this.label6.TabIndex = 9;
@@ -90,8 +133,9 @@
             // label5
             // 
             this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.ControlDark;
             this.label5.Font = new System.Drawing.Font("MS UI Gothic", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label5.Location = new System.Drawing.Point(57, 498);
+            this.label5.Location = new System.Drawing.Point(53, 489);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(63, 35);
             this.label5.TabIndex = 8;
@@ -99,14 +143,14 @@
             // 
             // progressBar4
             // 
-            this.progressBar4.Location = new System.Drawing.Point(841, 490);
+            this.progressBar4.Location = new System.Drawing.Point(841, 484);
             this.progressBar4.Name = "progressBar4";
             this.progressBar4.Size = new System.Drawing.Size(360, 43);
             this.progressBar4.TabIndex = 7;
             // 
             // progressBar3
             // 
-            this.progressBar3.Location = new System.Drawing.Point(122, 490);
+            this.progressBar3.Location = new System.Drawing.Point(122, 484);
             this.progressBar3.Name = "progressBar3";
             this.progressBar3.Size = new System.Drawing.Size(360, 43);
             this.progressBar3.TabIndex = 6;
@@ -114,8 +158,9 @@
             // label4
             // 
             this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.ControlDark;
             this.label4.Font = new System.Drawing.Font("MS UI Gothic", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label4.Location = new System.Drawing.Point(776, 168);
+            this.label4.Location = new System.Drawing.Point(776, 102);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(59, 35);
             this.label4.TabIndex = 5;
@@ -124,8 +169,9 @@
             // label3
             // 
             this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ControlDark;
             this.label3.Font = new System.Drawing.Font("MS UI Gothic", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label3.Location = new System.Drawing.Point(57, 168);
+            this.label3.Location = new System.Drawing.Point(57, 102);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(59, 35);
             this.label3.TabIndex = 4;
@@ -133,14 +179,14 @@
             // 
             // progressBar2
             // 
-            this.progressBar2.Location = new System.Drawing.Point(841, 160);
+            this.progressBar2.Location = new System.Drawing.Point(841, 94);
             this.progressBar2.Name = "progressBar2";
             this.progressBar2.Size = new System.Drawing.Size(360, 43);
             this.progressBar2.TabIndex = 3;
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(122, 160);
+            this.progressBar1.Location = new System.Drawing.Point(122, 94);
             this.progressBar1.Name = "progressBar1";
             this.progressBar1.Size = new System.Drawing.Size(360, 43);
             this.progressBar1.TabIndex = 2;
@@ -148,8 +194,9 @@
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ControlDark;
             this.label2.Font = new System.Drawing.Font("MS UI Gothic", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label2.Location = new System.Drawing.Point(835, 112);
+            this.label2.Location = new System.Drawing.Point(835, 56);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(126, 35);
             this.label2.TabIndex = 1;
@@ -158,26 +205,13 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ControlDark;
             this.label1.Font = new System.Drawing.Font("MS UI Gothic", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label1.Location = new System.Drawing.Point(116, 112);
+            this.label1.Location = new System.Drawing.Point(116, 56);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(126, 35);
             this.label1.TabIndex = 0;
             this.label1.Text = "Player1";
-            // 
-            // panel2
-            // 
-            this.panel2.Location = new System.Drawing.Point(122, 209);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(360, 275);
-            this.panel2.TabIndex = 11;
-            // 
-            // panel3
-            // 
-            this.panel3.Location = new System.Drawing.Point(841, 209);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(360, 275);
-            this.panel3.TabIndex = 12;
             // 
             // Form1
             // 
@@ -209,6 +243,8 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
     }
 }
 
