88b223ec262d776e58ba28b0fb9fa61efa89603b
Akie Ohno <akie.ohno@fujielab.org>
2016-08-07 10:30:35 +0900

    Kinectプログラムの書きかえ 腕と頭の位置を比較してパンチ判定が行えるようにした

--- Recognition/KinectTest/KinectConsole/Program.cs
+++ Recognition/KinectTest/KinectConsole/Program.cs
@@ -40,10 +40,27 @@ namespace KinectConsole
         ColorImagePoint[] colorImage;
         DepthImagePixel[] depthpixel;
 
+        DepthImageFormat depthformat;
+
         /// <summary>深度情報→ポイントクラウドに変換するためのCoorinateMapper</summary>
         private CoordinateMapper coordinateMappper;
 
         private ColorImageFormat colorImageformat;
+
+        //プレイヤー情報/////
+        private int[] player1 = new int[320 * 240];
+        private int[] player2 = new int[320 * 240];
+
+        private int kobusi1;
+        private int kobusi2;
+
+
+        bool guard1 = false;
+        bool guard2 = false;
+        //////////////
+
+
+
 
         /// <summary>
         /// Kinectのスタート
@@ -54,22 +71,23 @@ namespace KinectConsole
             this.kinectSensor1 = KinectSensor.KinectSensors[0];
 
             //二台目のKinectの接続
-            //this.kinectSensor2 = KinectSensor.KinectSensors[1];
+            this.kinectSensor2 = KinectSensor.KinectSensors[1];
 
             //深度情報の設定。スケルトンに直結する関数なので消さないように
             this.coordinateMappper = new CoordinateMapper(this.kinectSensor1);
 
             //カラーイメージの有効化・大きさの定義
             this.kinectSensor1.ColorStream.Enable(ColorImageFormat.InfraredResolution640x480Fps30);
-            //this.kinectSensor2.ColorStream.Enable(ColorImageFormat.InfraredResolution640x480Fps30);
+            this.kinectSensor2.ColorStream.Enable(ColorImageFormat.InfraredResolution640x480Fps30);
 
             //深度イメージの有効化・大きさの定義
             this.kinectSensor1.DepthStream.Enable(DepthImageFormat.Resolution640x480Fps30);
-           // this.kinectSensor2.DepthStream.Enable(DepthImageFormat.Resolution640x480Fps30);
+            this.kinectSensor2.DepthStream.Enable(DepthImageFormat.Resolution640x480Fps30);
+           
 
             //骨格トラッキングの有効化・大きさの定義
             this.kinectSensor1.SkeletonStream.Enable();
-            //this.kinectSensor2.SkeletonStream.Enable();
+            this.kinectSensor2.SkeletonStream.Enable();
 
             //colorImageの領域確保
             colorImage = new ColorImagePoint[this.kinectSensor1.ColorStream.FramePixelDataLength];
@@ -78,18 +96,19 @@ namespace KinectConsole
 
             
             //全てのFrameのイベントハンドラ
-            kinectSensor1.AllFramesReady += KinectSensor_AllFramesReady2;
-           // kinectSensor2.AllFramesReady += KinectSensor_AllFramesReady2;
+            kinectSensor1.AllFramesReady += KinectSensor_AllFramesReady;
+            kinectSensor2.AllFramesReady += KinectSensor_AllFramesReady2;
 
             //Kinectセンサーの起動
             this.kinectSensor1.Start();
 
             //Kinect2台目の起動
-            //this.kinectSensor2.Start();
+            this.kinectSensor2.Start();
         }
         
 
         /// <summary>
+        /// Kinect1
         /// 全てのフレームを統括したAllFramesReadyのイベント内容
         /// </summary>
         /// <param name="sender"></param>
@@ -103,57 +122,53 @@ namespace KinectConsole
                 {
                     using (SkeletonFrame skeletonFrame = e.OpenSkeletonFrame())
                     {
-
+                        //スケルトンの処理
+                        Skeleton[] skeleton = new Skeleton[skeletonFrame.SkeletonArrayLength];
+                        
+                       
                         // 深度画像の生データ（PlayerIndexなども入っている）の配列を取得
                         DepthImagePixel[] depthImagePixels = depthFrame.GetRawPixelData();
 
-                        // depthImagePixelsと同じ数だけのポイントデータを作成する
-                        if (this.skeletonPoints == null ||
-                            this.skeletonPoints.Length != depthImagePixels.Length)
+                        if (depthFrame != null)
                         {
-                            this.skeletonPoints = new SkeletonPoint[depthImagePixels.Length];
-
+                            for (int u = 0; u < depthImagePixels.Length; u++)
+                            {
+                                if (depthImagePixels[u].PlayerIndex != 0)
+                                {
+                                    player1[u] = 1;
+                                }
+                                else
+                                    player1[u] = 0;
+                            }
                         }
 
-                        // 深度ピクセルを変換して skeletonPoints にする
-                        this.coordinateMappper.MapDepthFrameToSkeletonFrame(
-                            depthFrame.Format, depthImagePixels, skeletonPoints);
-
-
-                        ///ここに各フレームに対してやりたい処理などを書いていく
-                        short[] pixel=new short[depthFrame.PixelDataLength];
-                        Skeleton[] skeleton = new Skeleton[skeletonFrame.SkeletonArrayLength];
-
-
-                        if (skeletonFrame!=null) {                            
-                            skeletonFrame.CopySkeletonDataTo(skeleton);
-
-                            Console.WriteLine(string.Format("{0},{1},{2},{3}",
-                                "left",
-                                skeleton[5].Position.X,
-                                skeleton[5].Position.Y,
-                                skeleton[5].Position.Z));
+                        //腕と頭のZ位置を比較する
+                        if (skeleton != null)
+                        {
+                            if (skeleton[1].Position.Z >= skeleton[4].Position.Z + 50.0)
+                            {
+                                Point a = SkeletonPointToScreen2(skeleton[4].Position);
+                                kobusi1 = a.X + (a.Y - 1) * 320;
+                            }
                         }
+                                                
                     }
                 }
             }//using破棄
 
         }
 
+
+
+
         /// <summary>
+        /// Kinect2
         /// 全てのフレームを統括したAllFramesReadyのイベント内容
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
         public void KinectSensor_AllFramesReady2(object sender, AllFramesReadyEventArgs e)
-        {
-            
-            string labelingFile = Path.Combine(
-              "C:/Users/jakky_2/Pictures/kobusi/player" + fn + ".txt");
-            StreamWriter sw = new StreamWriter(labelingFile, append: false, encoding: Encoding.UTF8);
-            int count = 0;
-            int val = 0;
-
+        {               
             //AllFrameReadyEvebtArgs内から、各要素のフレーム情報を取得することができる
             using (ImageFrame imageFrame = e.OpenColorImageFrame())
             {
@@ -161,42 +176,36 @@ namespace KinectConsole
                 {
                     using (SkeletonFrame skeletonFrame = e.OpenSkeletonFrame())
                     {
-                        int[] player2 = new int[640*480];
-                        int kobusi = 3000;
-                        bool guard =false;
-                        // 深度画像の生データ（PlayerIndexなども入っている）の配列を取得
-                        DepthImagePixel[] depthImagePixels = depthFrame.GetRawPixelData();
-
-
-                        for (int u = 0; u < depthImagePixels.Length; u++)
-                        {
-                            if (depthImagePixels[u].PlayerIndex != 0)
-                            {
-                                player2[u] = 1;
-                                //sw.Write("1");                               
-                            }
-                            else
-                                player2[u] = 0;
-                                //sw.Write("0");
-
-                        }
-                        //当たり判定
-                        this.AttackJudge(player2,kobusi,guard);
-                        
-                        sw.Close();
-                        sw.Dispose();
 
                         //スケルトンの処理
                         Skeleton[] skeleton = new Skeleton[skeletonFrame.SkeletonArrayLength];
-                        if (skeletonFrame != null)
-                        {
-                            skeletonFrame.CopySkeletonDataTo(skeleton);
+                                           
 
-                            Console.WriteLine(string.Format("{0},{1},{2},{3}",
-                                fn,
-                                skeleton[5].Position.X,
-                                skeleton[5].Position.Y,
-                                skeleton[5].Position.Z));
+
+                        // 深度画像の生データ（PlayerIndexなども入っている）の配列を取得
+                        DepthImagePixel[] depthImagePixels = depthFrame.GetRawPixelData();
+
+                        if (depthFrame != null)
+                        {
+                            for (int u = 0; u < depthImagePixels.Length; u++)
+                            {
+                                if (depthImagePixels[u].PlayerIndex != 0)
+                                {
+                                    player2[u] = 1;
+                                }
+                                else
+                                    player2[u] = 0;
+                            }
+                        }
+
+                        //腕と頭のZ位置を比較する
+                        if (skeleton != null)
+                        {
+                            if (skeleton[1].Position.Z >= skeleton[4].Position.Z + 50.0)
+                            {
+                                Point a = SkeletonPointToScreen2(skeleton[4].Position);
+                                kobusi2 = a.X + (a.Y - 1) * 320;
+                            }
                         }
                         
                     }
@@ -206,13 +215,51 @@ namespace KinectConsole
         }
 
 
-        ///////////////
+
+        /// <summary>
+        /// スケルトン情報をdepthのx,y座標に置き換える関数（１台目用）
+        /// </summary>
+        /// <param name="skelpoint">point to map</param>
+        /// <returns>mapped point</returns>
+        private Point SkeletonPointToScreen(SkeletonPoint skelpoint)
+        {
+            // Convert point to depth space.  
+            // We are not using depth directly, but we do want the points in our 640x480 output resolution.
+            DepthImagePoint depthPoint = this.kinectSensor1.CoordinateMapper.MapSkeletonPointToDepthPoint(skelpoint, DepthImageFormat.Resolution320x240Fps30);
+            return new Point(depthPoint.X, depthPoint.Y);
+        }
+
+
+        /// <summary>
+        /// スケルトン情報をdepthのx,y座標に置き換える関数（２台目用）
+        /// </summary>
+        /// <param name="skelpoint">point to map</param>
+        /// <returns>mapped point</returns>
+        private Point SkeletonPointToScreen2(SkeletonPoint skelpoint)
+        {
+            // Convert point to depth space.  
+            // We are not using depth directly, but we do want the points in our 640x480 output resolution.
+            DepthImagePoint depthPoint = this.kinectSensor2.CoordinateMapper.MapSkeletonPointToDepthPoint(skelpoint, DepthImageFormat.Resolution320x240Fps30);
+            return new Point(depthPoint.X, depthPoint.Y);
+        }
+
+
+
+
+
+        ///////////////     攻撃判定用       ////////////////////////////////////////////
+        /// <summary>
+        /// 人物位置と拳の位置、ガード状態を見て攻撃判定を行う関数
+        /// </summary>
+        /// <param name="player2"></param>
+        /// <param name="kobusi"></param>
+        /// <param name="guard"></param>
         public void AttackJudge(int[] player2, int kobusi, bool guard)
         {
             int m = 0;
-            int[] pos = new int[640 * 480];  //プレイヤーの領域
+            int[] pos = new int[320 * 240];  //プレイヤーの領域
 
-            for (int x = 0; x < 640 * 480; x++)   //配列の中身全回し
+            for (int x = 0; x < 320 * 240; x++)   //配列の中身全回し
             {
                 if (player2[x] == 1)
                 {
