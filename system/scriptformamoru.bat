d2a76054cab468e7347d9db8c7b86784cdce28c6
shibata-mamoru <mamoru.shibata@fujielab.org>
2016-08-08 11:40:28 +0900

    修正2

---
+++ system/scriptformamoru.bat
@@ -0,0 +1,3 @@
+start "root_system" "C:\Users\mamoru\FujielabFiters\system\RootSystem\RootSysytem\RootSysytem\bin\Release\RootSysytem.exe"
+start "kinect" "C:\Users\mamoru\FujielabFiters\Recognition\KinectTest\KinectConsole\bin\Release\KinectConsole.exe"
+start "UI" "C:\Users\mamoru\FujielabFiters\UI\FFgameUItest\FFgameUI\bin\Release\FFgameUI.exe"
