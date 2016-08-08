e6ad79e6eeb6ba633dc6fd84c514606b71272ad2
shibata-mamoru <mamoru.shibata@fujielab.org>
2016-08-08 09:47:51 +0900

    描画条件文の修正

--- UI/FFgameUItest/FFgameUI/Form1.cs
+++ UI/FFgameUItest/FFgameUI/Form1.cs
@@ -36,14 +36,14 @@ namespace FFgameUI
         Image.FromFile(@"C:\UIimg\man11.png"),   //ガード時
         Image.FromFile(@"C:\UIimg\man13.png"),  //必殺技
         Image.FromFile(@"C:\UIimg\man22.png")};  //ダメージ
-    Image showImage1;
+        Image showImage1;
         Image[] player2Image = new Image[] { //player2の素材
         Image.FromFile(@"C:\UIimg\2man.png"), //デフォルト画像
         Image.FromFile(@"C:\UIimg\2man14.png"),  //攻撃時
         Image.FromFile(@"C:\UIimg\2man11.png"),   //ガード時
         Image.FromFile(@"C:\UIimg\2man13.png"), //必殺技
         Image.FromFile(@"C:\UIimg\2man22.png")};  //ダメージ
-    Image showImage2;
+        Image showImage2;
         SoundPlayer gong = new SoundPlayer(@"C:\UIimg\gong.wav"); //ゴング音の読み込み
         SoundPlayer attack = new SoundPlayer(@"c:\UIimg\attack.wav");//アタック音声読み込み
         SoundPlayer guard = new SoundPlayer(@"c:\UIimg\guard.wav");//ガード音読み込み
@@ -61,7 +61,7 @@ namespace FFgameUI
             {
                 //DrawImageメソッドで画像を座標(0, 0)の位置に表示する
                 e.Graphics.DrawImage(backgroundImage,
-                    0, 0, backgroundImage.Width*2/5, backgroundImage.Height*2/5);
+                    0, 0, backgroundImage.Width * 2 / 5, backgroundImage.Height * 2 / 5);
             }
         }
 
@@ -90,7 +90,7 @@ namespace FFgameUI
 
             Update_A_HP update_a_hp = new Update_A_HP(); // Update_A_HPクラスインスタンスを作成
 
-            update_a_hp.systemstate += new EventHandler(this.gameover);　
+            update_a_hp.systemstate += new EventHandler(this.gameover);
             update_a_hp.update_A_HP += new EventHandler(this.A_HPupdate);
             update_a_hp.update_B_HP += new EventHandler(this.B_HPupdate);
             update_a_hp.drow += new EventHandler(this.drow);
@@ -106,14 +106,14 @@ namespace FFgameUI
         {
             string player_A_HP = "player_A_HP";
             progressBar1.Value = int.Parse(AccessClass.pull(player_A_HP));
-            
-            
+
+
         }
         private async void B_HPupdate(object sender, System.EventArgs e) //playerBのHP,MP更新
         {
             string player_B_HP = "player_B_HP";
             progressBar2.Value = int.Parse(AccessClass.pull(player_B_HP));
-            
+
         }
         private async void A_MPupdate(object sender, System.EventArgs e) //playerAのMP更新
         {
@@ -128,18 +128,18 @@ namespace FFgameUI
 
         private void panel2_Paint(object sender, PaintEventArgs e) //player1の画像表示
         {
-            if(showImage1 != null) //選択プレイヤー画像の描画
+            if (showImage1 != null) //選択プレイヤー画像の描画
             {
                 e.Graphics.DrawImage(showImage1,
-                    0, 20, showImage1.Width*2/3, showImage1.Height*2/3);
+                    0, 20, showImage1.Width * 2 / 3, showImage1.Height * 2 / 3);
             }
             else //デフォルト画像の描画
             {
                 showImage1 = player1Image[0];
                 e.Graphics.DrawImage(showImage1,
-                    0, 20, showImage1.Width*2/3, showImage1.Height*2/3);
+                    0, 20, showImage1.Width * 2 / 3, showImage1.Height * 2 / 3);
             }
-         
+
         }
 
         private void panel3_Paint_1(object sender, PaintEventArgs e) //player2の画像表示
@@ -147,17 +147,17 @@ namespace FFgameUI
             if (showImage2 != null) //選択プレイヤー画像の描画
             {
                 e.Graphics.DrawImage(showImage2,
-                   0, 30, showImage2.Width *2/ 3, showImage2.Height *2/ 3); //(描画オブジェクト, X, Y, 画像幅, 画像高さ)
+                   0, 30, showImage2.Width * 2 / 3, showImage2.Height * 2 / 3); //(描画オブジェクト, X, Y, 画像幅, 画像高さ)
             }
             else //デフォルト画像の描画
             {
                 showImage2 = player2Image[0];
                 e.Graphics.DrawImage(showImage2,
-                   0, 30, showImage2.Width *2/ 3, showImage2.Height *2/ 3);
+                   0, 30, showImage2.Width * 2 / 3, showImage2.Height * 2 / 3);
             }
         }
 
-       public async void drow(object sender, System.EventArgs e) // キャラクター描画、音声再生の関数
+        public async void drow(object sender, System.EventArgs e) // キャラクター描画、音声再生の関数
         {
             string player_A_move = "player_A_move";
             string player_B_move = "player_B_move";
@@ -294,39 +294,40 @@ namespace FFgameUI
                             panel2.Invalidate();
                         }
                     }
-                    else //　回避成功
+                }
+                else //　回避成功
+                {
+                    if (A_move == "attack") // A attack
                     {
-                        if (A_move == "attack") // A attack
+                        attack.Play();
+                        showImage1 = player1Image[1];
+                        panel2.Invalidate();
+                        await Task.Run(() =>
                         {
-                            attack.Play();
-                            showImage1 = player1Image[1];
-                            panel2.Invalidate();
-                            await Task.Run(() =>
-                            {
-                                Thread.Sleep(150);
-                            });
-                            showImage1 = player1Image[0];
-                            panel2.Invalidate();
-                        }
-                        if (B_move == "attack") // B attack
+                            Thread.Sleep(150);
+                        });
+                        showImage1 = player1Image[0];
+                        panel2.Invalidate();
+                    }
+                    if (B_move == "attack") // B attack
+                    {
+                        attack.Play();
+                        showImage2 = player2Image[1];
+                        panel3.Invalidate();
+                        await Task.Run(() =>
                         {
-                            attack.Play();
-                            showImage2 = player2Image[1];
-                            panel3.Invalidate();
-                            await Task.Run(() =>
-                            {
-                                Thread.Sleep(150);
-                            });
-                            showImage2 = player2Image[0];
-                            panel3.Invalidate();
-                        }
+                            Thread.Sleep(150);
+                        });
+                        showImage2 = player2Image[0];
+                        panel3.Invalidate();
                     }
                 }
-
             }
-            
-            
+
         }
+
+
+
 
         private async void gameover(object sender, EventArgs e)  //ゲーム終了時の描画
         {
@@ -360,13 +361,13 @@ namespace FFgameUI
         {
             if (progressBar2.Value != 0)
             {
-                
+
                 attack.Play();
                 progressBar2.Value -= 10;//プログレスバー値入力
                 showImage1 = player1Image[1];//プレイヤー１画像切り替え
                 showImage2 = player2Image[4];//プレイヤー２画像切り替え
                 panel1.Invalidate();// パネル再描画
-                await Task.Run(() =>　
+                await Task.Run(() =>
                 {
                     Thread.Sleep(150); //150ms? 待機
                 });
@@ -374,23 +375,23 @@ namespace FFgameUI
                 showImage2 = player2Image[0];
                 panel1.Invalidate();
             }
-            
+
 
         }
 
         private async void button3_Click(object sender, EventArgs e) //ガード動作チェック
         {
-                
-                guard.Play();
-                showImage1 = player1Image[2];
-                panel2.Invalidate();
-                
-                await Task.Run(() =>
-                {
-                    Thread.Sleep(150);
-                });
-                showImage1 = player1Image[0];
-                panel2.Invalidate();
+
+            guard.Play();
+            showImage1 = player1Image[2];
+            panel2.Invalidate();
+
+            await Task.Run(() =>
+            {
+                Thread.Sleep(150);
+            });
+            showImage1 = player1Image[0];
+            panel2.Invalidate();
         }
 
         private void panel4_Paint(object sender, PaintEventArgs e)
@@ -422,7 +423,6 @@ namespace FFgameUI
             while (n == 1)
             {
 
-                
                 if (AccessClass.update(UI, system_update, -1)) // systemのアップデートを確認
                 {
                     AccessClass.push(system_update, "False");
@@ -445,3 +445,4 @@ namespace FFgameUI
         }
     }
 }
+
