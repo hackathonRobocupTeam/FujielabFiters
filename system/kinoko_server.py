d585cef867d1962f1d9c4c02602c59b8b6684434
aokiyuya <yuya.aoki@fujielab.org>
2016-08-13 13:59:45 +0900

    kinoko_serverをソケット通信に変更。あとでライブラリも対応させる

--- system/kinoko_server.py
+++ system/kinoko_server.py
@@ -5,122 +5,129 @@
 #
 ###########################
 
+import socket
+import sys
 from threading import Thread
-# import datetime
-import time
 import copy
-import BaseHTTPServer
-import urlparse
-import Queue
+import time
+import client
 
-q = Queue.Queue()
-
+HOST = "localhost"   # Symbolic name meaning all available interfaces
+PORT = 2828          # Arbitrary non-privileged port
+stop_flag = True
+s = None
 list = {}
 modules = {}
 time_stamps = {}
-
-# request handler
-class HttpHandler(BaseHTTPServer.BaseHTTPRequestHandler):
-    """docstring for HttpHandler"""
-    # response を書く場所
-    def do_GET(self):
-        parsed_path = urlparse.urlparse(self.path)
-        try:        # 4は変数を見ている
-            paramater = dict([p.split('=') for p in parsed_path[4].split('%26')])
-        except:
-            paramater = {}
-        if len(paramater) > 0:
-            if 'push' in paramater['role']:
-                item = self.__push(paramater)
-            if 'pull' in paramater['role']:
-                item = self.__pull(paramater) 
-            if 'update' in paramater['role']:
-                item = self.__update(paramater)
-        else:
-            item = "Error"
-        print(item)
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write(item)
-
-    # リクエストに応じた情報を追加
-    def __push(self, paramater):
-        global list, time_stamps
-        if paramater['status_name'] in list:
-             list[paramater['status_name']] = paramater['status']
-             time_stamps[paramater['status_name']] = return_UNIX_time()
-
-        else:
-            list.update({paramater['status_name']: paramater['status']})
-            time_stamps.update({paramater['status_name']: return_UNIX_time() })
-        return 'SUCCESS'
-
-    # リクエストされた情報の現在の値を返す
-    def __pull(self, paramater):
-        global list
-        if paramater['status_name'] in list:
-            return list[paramater['status_name']]
-        else:
-            return 'NON_STATUS'
-
-    # 情報に更新があったかを見る
-    def __update(self, paramater):
-        global list, modules, time_stamps
-        module = paramater['module_name']
-        status_name = paramater['status_name']
-        # status がない場合登録
-        if not status_name in time_stamps:
-            time_stamps.update({status_name: return_UNIX_time()})
-        if module in modules:
-            if not status_name in modules[module]:
-                modules[module].update({status_name: return_UNIX_time()})
-            if paramater['time'] != "-1":
-                time.sleep(float(paramater['time']))
-            else : # 更新があるまでストップしたい
-                # while True:
-                #    if time_stamps[status_name] != modules[module][status_name]: return True
-                pass
-            # time_stampの比較
-            if modules[module][status_name] < time_stamps[status_name]:
-                modules[module][status_name] = copy.deepcopy(time_stamps[status_name])
-                return True
-            else:
-                return False
-
-        # moduleの登録
-        else:
-           modules.update({module: {status_name: copy.deepcopy(time_stamps[status_name])}})
-           return False
 
 
 def return_UNIX_time():
     return time.time()
 
+def sock_server():
+    global stop_flag
+    while stop_flag:
+        conn, addr = s.accept()
+        while 1:
+            # 受信バイト数。特に意味はない
+            data = conn.recv(2048)
+            param = dict([tmp.split('=') for tmp in data.split('&')])
+            if not data: break
+            # ここにdataからroleを引き出してそれぞれに応じた関数をとれるように書く
+            if 'role=update' in data:
+                __update(param, conn)
+            if 'role=pull' in data:
+                __pull(param, conn)
+            if 'role=push' in data:
+                __push(param, conn)
+            if "stop" in data:
+                sys.exit(0)
+    conn.close()
 
-def makeHttpServer(IP=None):
-    if IP is None:
-        server_address = ('', 2828)
+# リクエストに応じた情報を追加
+def __push(paramater, conn):
+    global list, time_stamps
+    if paramater['status_name'] in list:
+         list[paramater['status_name']] = paramater['status']
+         time_stamps[paramater['status_name']] = return_UNIX_time()
     else:
-        server_address = ()
-    httpd = BaseHTTPServer.HTTPServer(server_address, HttpHandler)
-    httpd.serve_forever()
+        list.update({paramater['status_name']: paramater['status']})
+        time_stamps.update({paramater['status_name']: return_UNIX_time() })
+    res = 'SUCCESS'
+    conn.send(res)
 
+# リクエストされた情報の現在の値を返す
+def __pull(paramater, conn):
+    global list
+    if paramater['status_name'] in list:
+        res = list[paramater['status_name']]
+    else:
+        res = 'NON_STATUS'
+    conn.send(res)
 
-if __name__ == '__main__':
-    # code
-    # ctrl-C で止めるために他のコマンドを廃止
-    makeHttpServer()
-    t = Thread(target=makeHttpServer)
-    t.damon = True
-    t.start()
-    while True:
-        cmd = raw_input()
-        if "list" == cmd:
-            print (list)
-        if 'modules' == cmd:
-            print(modules)
-        if 'times' == cmd:
-            print time_stamps
-        if 'stop' == cmd:
-            q.put("stop")
-            print "put"
+# 情報に更新があったかを見る
+def __update(paramater, conn):
+    global list, modules, time_stamps
+    module = paramater['module_name']
+    status_name = paramater['status_name']
+    # status がない場合登録
+    if not status_name in time_stamps:
+        time_stamps.update({status_name: return_UNIX_time()})
+    if module in modules:
+        if not status_name in modules[module]:
+            modules[module].update({status_name: return_UNIX_time()})
+        if paramater['time'] != "-1":
+            time.sleep(float(paramater['time']))
+        else : # 更新があるまでストップしたい
+            while True:
+               if time_stamps[status_name] != modules[module][status_name]: return True
+        # time_stampの比較
+        if modules[module][status_name] < time_stamps[status_name]:
+            modules[module][status_name] = copy.deepcopy(time_stamps[status_name])
+            res = True
+        else:
+            res = False
+   # moduleの登録
+    else:
+       modules.update({module: {status_name: copy.deepcopy(time_stamps[status_name])}})
+       res = False
+    conn.send(res)
+
+# initialize
+for res in socket.getaddrinfo(HOST, PORT, socket.AF_UNSPEC,
+                              socket.SOCK_STREAM, 0, socket.AI_PASSIVE):
+    af, socktype, proto, canonname, sa = res
+    try:
+       s = socket.socket(af, socktype, proto)
+    except socket.error, msg:
+       s = None
+       continue
+    try:
+       s.bind(sa)
+       s.listen(1)
+    except socket.error, msg:
+       s.close()
+       s = None
+       continue
+    break
+if s is None:
+    print 'could not open socket'
+    sys.exit(1)
+
+t = Thread(target=sock_server)
+t.damon = True
+t.start()
+while True:
+     cmd = raw_input()
+     if "list" == cmd:
+        print (list)
+     if 'modules' == cmd:
+        print(modules)
+     if 'times' == cmd:
+        print time_stamps
+     if 'stop' == cmd:
+        stop_flag = False
+        print "stop"
+        client.stop()
+        sys.exit(0)
+
